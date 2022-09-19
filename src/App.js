50b3bc405549c4b91e3cb52f0d1326ef8950b412
Nice <puma1800@naver.com>
2022-09-20 01:12:29 +0900

    blog

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   let [글제목, 글제목변경] = useState( ['남자코트 추천', '강남우동 맛집', '파이썬 독학'] );
   let [따봉, 따봉변경] = useState([0,0,0]);
   let [modal, setmodal] = useState(false);
+  let [title, settitle] = useState(0);
+  let [입력값, 입력값변경] = useState(0);
   
   return (
     <div className="App">
@@ -26,44 +28,39 @@ function App() {
         정렬.sort();
         글제목변경(정렬);
         }}>정렬해요</button>
-    
-      
-      {/* <div className='list'>
-        <h4>{ 글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>좋아요 😀</span> {따봉} </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className='list'>
-        <h4>{ 글제목[1] }</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className='list'>
-        <h4 onClick={()=>{setmodal(true)}}>{ 글제목[2] }</h4>
-        <p onClick={()=>{setmodal(false)}}>2월 17일 발행</p>
-      </div>  */}
+
     {
       글제목.map(function(a,i){
         return (
         <div className='list' key='i'>
-        <h4 onClick={()=>{setmodal(true)}}>{글제목[i]} <span onClick={()=>{let copy=[...따봉]; copy[i]=copy[i]+1; 따봉변경(copy)}}>좋아요 😀
+        <h4 onClick={()=>{setmodal(true); settitle(i);}}>{글제목[i]} <span onClick={()=>{let copy=[...따봉]; 
+          copy[i]=copy[i]+1; 따봉변경(copy)}}>좋아요 😀
         </span> {따봉[i]} </h4>
         <p onClick={()=>{setmodal(false)}}>2월 17일 발행</p>
+        <button onClick={()=>{let copy=[...글제목]; copy.splice(i,1); 글제목변경(copy);}}>삭제</button>
       </div>
       )
       })
     }
-      {modal==true? <Modal 글제목={글제목}></Modal>: null}
+      {modal=true? <Modal title={title} 글제목변경={글제목변경} 글제목={글제목}></Modal>: null}
+
+      <input type='text' onChange={(e)=>{입력값변경(e.target.value)}}></input>
+      <button onClick={(e)=>{let copy= [...글제목]; copy.unshift(입력값); 글제목변경(copy);}}>추가</button>
     </div>
   );
 }
 
 const Modal = (props)=>{ return(
     <div className='modal'>
-    <h4>{props.글제목[0]}</h4>
+    <h4>{props.글제목[props.title]}</h4>
     <p>날짜</p>
     <p>상세내용</p>
+    <button onClick={()=>{let copy=[...props.글제목]; copy[0]='여자코트 추천'; props.글제목변경(copy);}}>글수정</button>
   </div>
 )
 };
+
+
 
 
 
